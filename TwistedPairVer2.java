857b7c184d25f7dbe15c87f9edc8d910cf9cac03
dangodfrey <c.daniel.godfrey@gmail.com>
2011-03-13 00:35:11 -0500

    Solution - Pre Comments and Clean Up

--- TwistedPairVer2.java
+++ TwistedPairVer2.java
@@ -12,6 +12,10 @@ public class TwistedPairVer2
 	private int tpId;   // identifier for twisted pair
 	private final int maxBufLen = 60;  // Maximum size of string to represent xmission accross twisted pair
 	private String buf;   // String to represent a twisted pair xmission, when empty it references an Empty String ""
+	private Semaphore blockedReaders = new Semaphore(0);
+	private Semaphore blockedWriters = new Semaphore(0);
+	private int blockedReaderCount = 0;
+	private int blockedWriterCount = 1;
 	/**
 	 * Constructor
 	 */
@@ -35,12 +39,28 @@ public class TwistedPairVer2
 	{
 		// Can we xmit?
 
-
+		while(msg.length() > (maxBufLen - buf.length()) )
+		{  
+			try
+			{
+				this.blockedWriterCount++;
+				blockedWriters.acquire();
+				this.blockedWriterCount--;
+			}
+			catch (InterruptedException ex)
+			{
+				this.logMsg("terminated"); //log that an interrupt occurred at this level
+				throw ex; //This needs to be handled further up (in the worker method) so we re-throw the error
+			}
+		}
 
 		// ---- Critical Section ------------------------------------
 		buf = buf+msg;  // appends new frame to the twisted pair
 		//-----------------------------------------------------------
-
+		if (this.blockedReaderCount > 0)
+		{
+			this.blockedReaders.release();
+		}
 
 	}
 	/*
@@ -49,14 +69,39 @@ public class TwistedPairVer2
 	public String recv() throws InterruptedException
 	{
 		String msgs;  // for returning string in twisted pair
-
+		
+		/*
+		  	While the buffer contains the empty string, there is nothing to be received ("consumed")
+		  	therefore, the recv method waits (releasing the monitor) until the xmit method ("producer")
+		  	sends a notification that something has been added to the buffer (in other words, something 
+		  	has been "produced") 
+		 */
+		
+		while (buf == "")
+		{	
+			try
+			{
+				this.blockedReaderCount++;
+				blockedReaders.acquire();
+				this.blockedReaderCount--;
+			}
+			catch (InterruptedException ex)
+			{
+				this.logMsg("terminated"); //log that an interrupt occurred at this level
+				throw ex; //This needs to be handled further up (in the worker method) so we re-throw the error
+			}
+		}
 
 		// ---- Critical Section ------------------------------------
 		msgs = buf;
 		buf = "";
-		//-----------------------------------------------------------
-
-
+		//-----------------------------------------------------------	
+		
+		for(int i=0;i<this.blockedWriterCount;i++)
+		{
+			this.blockedWriters.release();
+		}
+		
 		return(msgs);
 	}
 	
